44287d53bf38ac6858cedbbf8d01bd336d5d2f6b
Raamo <romik.kelesh@.com>
2025-08-11 21:40:53 +0200

    Endless loop

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from bot import run_driver
 import pandas as pd
 from streams_running_check.image_sequence import screenshot_similarity
@@ -7,24 +8,27 @@ df = pd.read_csv("production_streams.csv")
 data = [{"url": row["url"], "label": row["label"]} for index, row in df.iterrows()]
 
 if __name__ == "__main__":
-    for row in data:
-        # Init variables
-        url, label = row["url"], row["label"]
-        # Run the driver and take screenshots
-        if run_driver(url):
-            print("Driver ran successfully and screenshots were taken.")
-        else:
-            print("There was an error running the driver or taking screenshots.")
-        print("Checking screenshot similarity...")
-        if screenshot_similarity():
-            content = f"""
-            LABEL: {label}
-            STREAM: {url} 
-            It’s not working, no changes are happening in the video.
-            """
-            slack_send_message(content)
-            print(content)
-            print("-"*30)
-        else:
-            print("All streams are running fine, no changes detected.")
-    slack_send_message("All streams have been checked.")
+    while True:
+        print("Checking streams...")
+        time.sleep(60)
+        for row in data:
+            # Init variables
+            url, label = row["url"], row["label"]
+            # Run the driver and take screenshots
+            if run_driver(url):
+                print("Driver ran successfully and screenshots were taken.")
+            else:
+                print("There was an error running the driver or taking screenshots.")
+            print("Checking screenshot similarity...")
+            if screenshot_similarity():
+                content = f"""
+                LABEL: {label}
+                STREAM: {url} 
+                It’s not working, no changes are happening in the video.
+                """
+                slack_send_message(content)
+                print(content)
+                print("-"*30)
+            else:
+                print("All streams are running fine, no changes detected.")
+        slack_send_message("All streams have been checked.")
